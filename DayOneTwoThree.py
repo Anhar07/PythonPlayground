fb4f120f3a020aa27fceb59e3b82b6d72a0509cb
Anhar07 <anharsaif07@gmail.com>
2016-12-24 12:38:24 -0800

    dgkj

---
+++ DayOneTwoThree.py
@@ -0,0 +1,91 @@
+#Website I am using to Learn : https://docs.python.org/3/tutorial/introduction.html#using-python-as-a-calculator
+
+#playing with print
+print ("hellooooo")
+print ("i wonder why they chaged i nmm")
+print ("alrighty kms lmfao")
+
+#MATH YEAh
+print ("time to test this out", 5%2)
+print ("kfdnkkjf", 7 / 4)
+
+#VARIABLESSSSS 
+x = 5
+y = 7
+z = 2
+
+x - y
+z + x
+
+print ("there is", x, "apples")
+print ("oomfg", y, "dfvf")
+
+#%s or %d(num)
+eyes = "brown"
+age = 15
+height = 60
+
+print (" my age is %s." %age)
+print ("my eye color is %s" %eyes)
+#(not sure why it wont work)
+#print ("I am %d inches and have %s colored eyes and is age %d" %(height, eyes, age)   
+#print ('Hi, your first name is %s and your last name is %s' %(Fname, Lname))
+
+
+#Day 2
+
+print (2**3) 
+
+#concanated string
+print (3 * "hello" + "yee")
+print ("py" "ufb")
+
+#strig indexed/ subscripted
+word = "Python"
+print (word[0])
+print (word[-1])
+print(word[0:2])
+print(word[:2] + word[4:])
+print (len(word))
+
+#Lists
+squares = [1, 4, 9, 16, 25]
+print(squares)
+print(squares[4:])
+print (squares + [4, 3, 4])
+cubes = [6,5,4,3,2]
+print (cubes)
+print(cubes[2:])
+cubes.append(45)
+print(cubes)
+cubes.append(345)
+print (squares + [4, 3, 4])
+print(cubes)
+#next
+letters = [1,2,3,4,5,6,7,8.9]
+print(letters)
+letters[0:3] = [8,8,8]
+print(letters)
+letters[0:3] = []
+print(letters)
+#letters[:] = []
+#print(letters)
+print (len(letters))
+
+# Fibonacci Series: The sum of two elements defines the next
+a, b = 0, 1		#Defining variables a and b
+while b < 10:	#While loop
+	print(b)	
+	a, b = b, a+b
+	
+
+	dfbjdjfbh
+#in C and python 0 is false, everything else is true
+
+a, b = 0, 1		#Defining variables a and b
+while b < 10:	#While loop
+	print(b)	
+	a, b = b, a+b
+	
+
+print("hello world")
