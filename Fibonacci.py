8fe22b9d1f5c7daa7c038685211ca4caf78948a4
Anhar07 <anharsaif07@gmail.com>
2016-12-24 15:27:04 -0800

    ??

---
+++ Fibonacci.py
@@ -0,0 +1,13 @@
+# Fibonacci Series: The sum of two elements defines the next
+a, b = 0, 1		#Defining variables a and b
+while b < 10:	#While loop, the indention shows what is insdife of the loop
+	print(b)	
+	a, b = b, a+b
+#Basically a = b, 1 = 1: 
+#b is a +b so, 1 + 1
+#
+	
+
+
+#in C and python 0 is false, everything else is true
+
