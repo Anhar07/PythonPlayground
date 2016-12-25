edba12e526c097ee4c3b244e65da50848956c981
Anhar07 <anharsaif07@gmail.com>
2016-12-25 09:05:20 -0800

    added more

--- Chapt4.py
+++ Chapt4.py
@@ -22,6 +22,37 @@ else:
 #For Statements
 #MEasuring strings
 words = ['cat', 'window', 'defenestrate']
+print(words[:])
 for i in words:		#it doesnt matter what letter you use as long as it is consistent
 	print(i, len(i)) #i = words. so it prints cat 3, pretty cool
 #another for statement
+for w in words [:]: #i guess it is for looping list
+	if len(w) > 6: #command
+		words.insert(0, w) #.insert() for any LISTS
+print(words)
+
+#Range() function
+
+for i in range(5): #iterating
+	print(i)
+
+print ("BR E SA K  O F F")
+
+for i in range(0, 10, 3): #called a step
+	print(i)
+
+print ("BR E SA K  O F F")
+
+for i in range(5,9): #called a step
+	print(i)
+
+print ("BR E SA K  O F F")
+
+for i in range(-10, -100, -30): #i dont understand why theres 3 but i'm feeling lazy spppp yeah
+	print(i)
+
+
+
+
+
+
